3db73ee8ea5c9d671d665dccba9710846df944f6
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2022-06-19 18:08:16 +0300

    feat: finish first scenario

--- CRUD-api/test/firstScenario.js
+++ CRUD-api/test/firstScenario.js
@@ -5,13 +5,13 @@ const chaiHttp = require('chai-http');
 const server = require('../build/index.js').default;
 const should = chai.should();
 
-
 chai.use(chaiHttp);
 // Our main block
 describe('Users', () => {
   // Consts
   const successCode = 200;
   const deleteCode = 204;
+  const errorCode = 404;
   const user = {
       username: 'Kiryl',
       age: 22,
@@ -21,14 +21,14 @@ describe('Users', () => {
     username: 'Roman',
     age: 23,
     hobbies: ['java', 'js'],
-};
+  };
+  const fakeUUID = '6c56a54e-efd5-4642-b41f-6bdc5e8d507b';
 
-  const testPrice = { title: 'hello', price: '778' };
   /*
   * Test for /GET
   */
-  describe('/GET product', () => {
-    it('it should GET all the products', done => {
+  describe('/GET user', () => {
+    it('it should GET all the users', done => {
       chai.request(server)
         .get('/api/users')
         .end((err, res) => {
@@ -118,13 +118,30 @@ describe('Users', () => {
         .send(user)
         .end((err, res) => {
           let testId = res.body.id;
-          console.log(testId)
           chai.request(server)
               .delete(`/api/users/${testId}`)
               .end((err, res) => {
                 res.should.have.status(deleteCode);
                 done();
               });
+        });
+    });
+  });
+  /*
+  * Test for GET api/users/{userId}
+  */
+  describe('/GET/:non-existent id user ', async () => {
+    it('it should GET a user by the given non-existent id', done => {
+      chai.request(server)
+        .post('/api/users')
+        .send(user)
+        .end((err, res) => {
+          chai.request(server).get(`/api/users/${fakeUUID}`)
+            .end((err, res) => {
+              res.should.have.status(errorCode);
+              res.body.should.have.property('message').eql(`User Not Found`);
+            done();
+          });
         });
     });
   });
